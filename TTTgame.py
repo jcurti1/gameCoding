aaacd520c9f1e4547838566dcc086968acaf4cdb
Jovon Curtis <JovonCurtis@gmail.com>
2017-05-07 17:18:24 -0700

    add instructions function

--- TTTgame.py
+++ TTTgame.py
@@ -1,3 +1,29 @@
-print("Hello World!")
-print("Getting Ready to Start Coding Game")
-print("Got a fairly good understanding of GitHub")
+# Creating a Tic Tac Toe Game
+
+#Instructions function for Tic Tac Toe Game
+def instructions():
+    """Displaying the games instructions"""
+    print("Welcome to Tic Tac Toe!!\n"
+          "The Instructions to how you play the game are shown below.\n"
+          "This game will be between the computer and your human brain\n"
+          "You will make your move by entering a number between 0 - 8\n"
+          "The number will represent the postion on the board as illustrated below:\n"
+          "0 | 1 | 2\n"
+          "---------\n"
+          "3 | 4 | 5\n"
+          "---------\n"
+          "6 | 7 | 8\n\n"
+          "Test yourself as the game is about to begin.\n")
+
+def testOne():
+    print(
+        """
+        Here is a basic test.
+        To see if the endline.
+        Is working right.
+        BOOM YAY!!!
+        """
+    )
+
+#main start code
+instructions()
